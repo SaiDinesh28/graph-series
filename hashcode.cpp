443e5bc56478b39af17f358adbffd0b8fe8ca33f
SaiDinesh28 <koradasaidinesh882@gmail.com>
2022-06-30 21:00:34 +0530

    infix to postfix including braces

--- hashcode.cpp
+++ hashcode.cpp
@@ -1,38 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 bool isOperand(char ch){
-  if(ch=='+' || ch=='-' || ch=='*' || ch=='/') return false;
+  if(ch=='+' || ch=='-' || ch=='*' || ch=='/' || ch=='^' || ch=='(' || ch==')') return false;
   return true;
 }
-int precedence(char ch){
+int outStackPrecedence(char ch){
   if(ch=='+' || ch=='-') return 1;
-  if(ch=='*' || ch=='/') return 2;
+  if(ch=='*' || ch=='/') return 3;
+  if(ch=='^') return 6;
+  if(ch=='(') return 7;
   return 0;
 }
-string infixToPostfix(string infix){
-  int i=0;
-  string postfix="";
+int inStackPrecedence(char ch){
+  if(ch=='+' || ch=='-') return 2;
+  if(ch=='*' || ch=='/') return 4;
+  if(ch=='^') return 5;
+  return 0;
+}
+string infixToPostfix(string s){
   stack<char>st;
-  while(i<infix.size()){
-     if(isOperand(infix[i])) {
-      postfix+=infix[i];
-      i++;
+  int i=0;
+  string ans="";
+  while(i<s.size()){
+    if(isOperand(s[i])) ans+=s[i++];
+    else if(st.empty() || outStackPrecedence(s[i])>inStackPrecedence(st.top())) {if(s[i]!=')') st.push(s[i]); i++;}
+    else {
+     if(!st.empty() && outStackPrecedence(s[i])<=inStackPrecedence(st.top())) {
+       if(st.top()!='(') ans+=st.top();
+      else i++;
+      st.pop();
      }
-     else{
-      if(st.empty() || precedence(infix[i])>precedence(st.top())) {
-        st.push(infix[i]);i++;
-      }
-      else{
-        postfix+=st.top();
-        st.pop();
-      }
-     }
+    }
   }
   while(!st.empty()) {
-    postfix+=st.top();
-    st.pop();
+    ans+=st.top();st.pop();
   }
-  return postfix;
+return ans;
 }
 int main(){
   string s;
